0c8d39bc2ad415e4df0779033b241ad678eaca5c
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2026-05-07 09:12:55 +0900

    feat: sort multiple playlists from sort_playlists.txt

--- run_sort.sh
+++ run_sort.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cd /Users/yoshihide/my-projects/spotify-playlist-tools
 LOG=run.log
+PYTHON=/Users/yoshihide/.pyenv/versions/spotify-playlist-tools-3.11.9/bin/python
+PLAYLISTS=sort_playlists.txt
 
-output=$(/Users/yoshihide/.pyenv/versions/spotify-playlist-tools-3.11.9/bin/python \
-  playlist.py \
-  "https://open.spotify.com/playlist/3gWeVkYJPREpkdCpDRjHFw?si=cdbafcbffb9e4b9a" 2>&1)
-exit_code=$?
-
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] sort exit=$exit_code" >> "$LOG"
-echo "$output" >> "$LOG"
+while IFS= read -r url || [[ -n "$url" ]]; do
+    [[ -z "$url" || "$url" == \#* ]] && continue
+    output=$("$PYTHON" playlist.py "$url" 2>&1)
+    exit_code=$?
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] sort exit=$exit_code $url" >> "$LOG"
+    echo "$output" >> "$LOG"
+done < "$PLAYLISTS"
